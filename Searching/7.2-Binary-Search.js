9bd23296ed4e356b2a2c368520ab18024e344fb7
Arman-Kabir <armankabir8@gmail.com>
2023-02-28 13:50:05 +0600

    binary search

---
+++ Searching/7.2-Binary-Search.js
@@ -0,0 +1,15 @@
+
+// 7.2
+// Binary Search will take less time than the linear search
+/*
+    Requirements of BS is - ****array should be sorted. But, That is not a case in linear search.
+
+    *** BS algorithm is divide and conquer technique.It means it will divide the array into two halves.
+    *Recursively divide the array into sub-arrays.
+
+    *We'r going to find the middle element of the array.For this, we take two variables
+     - left and right variables. Left at 0 and Right at n-1. Mid = floor(L+R)/2
+
+    If the data is not sorted , then u can't apply binary search on that array. First, sort the array.
+     Then     u can apply BS on that array.
+*/
