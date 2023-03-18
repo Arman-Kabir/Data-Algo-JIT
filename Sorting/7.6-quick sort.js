f4f2e4a724f9f712b4df77d027fe1a441d8a5372
Arman-Kabir <armankabir8@gmail.com>
2023-03-18 17:36:23 +0600

    doing quicksort

---
+++ Sorting/7.6-quick sort.js
@@ -0,0 +1,8 @@
+/**
+ * Quick sort algorithm is based on divide and conquer technique.
+ * 
+ * IN divide and conquer technique, the complete array should be divided into sub-arrays.Dividing the array into sub-arrays is called partitioning. Partioning of the array is backbone of quick sort.
+ * 
+ * *Partition exchange sort 
+ *  
+ */
